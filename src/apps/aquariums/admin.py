d55e8aef71c9ad66b5b5493d944f685f707ecdd8
sidnou <sidjack972@gmail.com>
2026-03-01 23:02:12 +0100

    Création du projet Gestionnaire Aquarium  - Création des models pour aquariums    * Aquarium    * Analyse    * ChangeEau    * Traitement

--- src/apps/aquariums/admin.py
+++ src/apps/aquariums/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Aquarium,Analyse,ChangeEau,Traitement
 # Register your models here.
+
+@admin.register(Aquarium)
+class AquariumAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Analyse)
+class AnalyseAdmin(admin.ModelAdmin):
+    pass
+@admin.register(ChangeEau)
+class ChangeEauAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Traitement)
+class TraitementAdmin(admin.ModelAdmin):
+    pass
--- src/apps/aquariums/models.py
+++ src/apps/aquariums/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+class Aquarium(models.Model):
+    nom = models.CharField(max_length=25)
+    volume_eau = models.IntegerField()
+    date_mise_en_service = models.DateField()
+    commentaire = models.TextField()
+    def __str__(self):
+        return self.nom
+class Analyse(models.Model):
+    aquarium = models.ForeignKey(Aquarium,on_delete=models.CASCADE)
+    no2 = models.FloatField(max_length=5)
+    no3 = models.FloatField(max_length=5)
+    ph = models.FloatField(max_length=5)
+    nh4 = models.FloatField(max_length=5)
+    kh = models.FloatField(max_length=5)
+    gh = models.FloatField(max_length=5)
+    sio2 = models.FloatField(max_length=5)
+    po4 = models.FloatField(max_length=5)
+    mg = models.FloatField(max_length=5)
+    fe = models.FloatField(max_length=5)
+    cu = models.FloatField(max_length=5)
+    observation = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.aquarium} {self.no2} "
+
+
+class ChangeEau(models.Model):
+    aquarium = models.ForeignKey(Aquarium,on_delete=models.CASCADE)
+    date_change_eau  = models.DateField()
+    quantite_litre = models.IntegerField()
+    osmose_pourcentage = models.IntegerField()
+    robinet_pourcentage = models.IntegerField()
+
+class Traitement(models.Model):
+    aquarium = models.ForeignKey(Aquarium,on_delete=models.CASCADE)
+    nom_produit = models.CharField(max_length=25)
+    quantite = models.IntegerField( help_text="en ml")
+    date_traitement = models.DateField()
+
+
