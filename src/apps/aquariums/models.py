4daf705e0e37982ad1b9e716b9e78afb5cb9c3b6
jasid <sidjack972@gmail.com>
2026-03-02 17:52:53 +0100

    Création du projet Gestionnaire Aquarium - Ajout models pour aquariums  * Espece  * Plante

--- src/apps/aquariums/models.py
+++ src/apps/aquariums/models.py
@@ -41,4 +41,30 @@ class Traitement(models.Model):
     quantite = models.IntegerField( help_text="en ml")
     date_traitement = models.DateField()
 
+class Espece(models.Model):
+    # TODO : Choix durtée de l'eau
+    # DURTEE_CHOIX = [
+    #
+    #     ]
+    aquarium = models.ForeignKey(Aquarium,on_delete=models.CASCADE)
+    nom_espece = models.CharField(max_length=25)
+    type_espece = models.CharField(max_length=25)
+    nombre_espece = models.IntegerField()
+    paramettre_min_ph = models.FloatField()
+    paramettre_max_ph = models.FloatField()
+    paramettre_eau = models.CharField(max_length=25)
+class Plante(models.Model):
+    # TODO : Choix d'Emplacemnt
+    # EMPLACEMENT_CHOIX = [
+    #
+    #     ]
+    aquarium = models.ForeignKey(Aquarium,on_delete=models.CASCADE)
+    nom_plante = models.CharField(max_length=25)
+    emplacement_aquarium = models.CharField(max_length=2)
+    paramettre_min_ph = models.FloatField()
+    paramettre_max_ph = models.FloatField()
+    paramettre_min_co2 = models.FloatField()
+    paramettre_max_co2 = models.FloatField()
+
+
 
