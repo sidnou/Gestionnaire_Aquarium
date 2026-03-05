a89385a38241dbc18ad29090bd3dd0ecb4412a11
jasid <sidjack972@gmail.com>
2026-03-05 17:00:55 +0100

    Ajout des fichiers de configuration de base, des modèles de vues et des routes pour le gestionnaire d'aquarium

--- src/apps/aquariums/views.py
+++ src/apps/aquariums/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def accueil(request):
+    context = {
+        "titre": "Bienvenue sur le gestionnaire d'aquarium",
+        "version": "0.0.1dev",
+
+    }
+
+    return render(request, 'aquariums/index.html',context)
+
+def tableau_bord(request):
+    context = {
+        "titre": "Tableau de bord",
+        "version": "0.0.1dev",
+
+    }
+
+    return render(request, 'aquariums/dashboard.html',context)
