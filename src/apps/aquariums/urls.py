5b258fc9f5d615ca7e4275044490afa839890418
sidnou <sidjack972@gmail.com>
2026-03-07 22:40:59 +0100

    Mise à jour de l'importation de la vue tableau_bord dans urls.py

--- src/apps/aquariums/urls.py
+++ src/apps/aquariums/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import tableau_bord
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
