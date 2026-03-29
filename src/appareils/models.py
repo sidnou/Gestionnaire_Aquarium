3da56027d8c0d704203de50f4b6eff67e49fb6b3
sidnou <sidjack972@gmail.com>
2026-03-29 23:56:12 +0200

    Add initial Django app structure with models, views, and templates for managing devices

---
+++ src/appareils/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+from aquariums.models import Aquarium
+
+
+# Create your models here.
+class Appareil(models.Model):
+    nom_appreil = models.CharField(max_length=100)
+    type_appareil = models.CharField(max_length=100)
+    aquarium = models.ForeignKey(Aquarium,on_delete=models.CASCADE)
+    adresse_ip = models.GenericIPAddressField(null=True,blank=True)
+    date_ajout = models.DateField(null=True,blank=True)
+    active = models.BooleanField(default=True)
